17a5e2cd1ef8ee2f9f612e05897a3ecd2b9b064d
Mulagada Venkata Akhil <138915096+git-akhi3@users.noreply.github.com>
2026-04-30 17:33:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const apiTarget = env.VITE_PROXY_TARGET || 'http://localhost:8000'
