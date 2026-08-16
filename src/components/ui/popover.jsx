537fe4aeaf8389bab004087508384d4b3c2cb974
Mulagada Venkata Akhil <git-akhi3@users.noreply.github.com>
2026-08-17 03:45:02 +0530

    update

--- src/components/ui/popover.jsx
+++ src/components/ui/popover.jsx
@@ -4,6 +4,7 @@ import { cn } from '../../lib/utils';
 
 const Popover = PopoverPrimitive.Root;
 const PopoverTrigger = PopoverPrimitive.Trigger;
+const PopoverAnchor = PopoverPrimitive.Anchor;
 
 const PopoverContent = React.forwardRef(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
   <PopoverPrimitive.Portal>
@@ -21,4 +22,4 @@ const PopoverContent = React.forwardRef(({ className, align = 'center', sideOffs
 ));
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
 
-export { Popover, PopoverTrigger, PopoverContent };
+export { Popover, PopoverTrigger, PopoverAnchor, PopoverContent };
